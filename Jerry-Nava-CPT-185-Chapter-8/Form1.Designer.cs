0e79db3cbd3311332081c3656be4e642fbec0b9e
navaJERRY <72098565+navaJERRY@users.noreply.github.com>
2021-07-20 17:11:10 -0400

    Add files via upload
    Morse Code Translator based on user input

---
+++ Jerry-Nava-CPT-185-Chapter-8/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Jerry_Nava_CPT_185_Chapter_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Exitbutton = new System.Windows.Forms.Button();
+            this.Convertbutton = new System.Windows.Forms.Button();
+            this.MorsecodelistBox = new System.Windows.Forms.ListBox();
+            this.StandardtextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Clrbutton = new System.Windows.Forms.Button();
+            this.Soundbutton = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // Exitbutton
+            // 
+            this.Exitbutton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.Exitbutton.Location = new System.Drawing.Point(376, 219);
+            this.Exitbutton.Margin = new System.Windows.Forms.Padding(4);
+            this.Exitbutton.Name = "Exitbutton";
+            this.Exitbutton.Size = new System.Drawing.Size(112, 34);
+            this.Exitbutton.TabIndex = 3;
+            this.Exitbutton.Text = "E&xit";
+            this.toolTip1.SetToolTip(this.Exitbutton, "Exit");
+            this.Exitbutton.UseVisualStyleBackColor = true;
+            this.Exitbutton.Click += new System.EventHandler(this.Exitbutton_Click);
+            // 
+            // Convertbutton
+            // 
+            this.Convertbutton.Location = new System.Drawing.Point(20, 219);
+            this.Convertbutton.Margin = new System.Windows.Forms.Padding(4);
+            this.Convertbutton.Name = "Convertbutton";
+            this.Convertbutton.Size = new System.Drawing.Size(112, 32);
+            this.Convertbutton.TabIndex = 1;
+            this.Convertbutton.Text = "&Convert";
+            this.toolTip1.SetToolTip(this.Convertbutton, "Convert Text To Morse Code");
+            this.Convertbutton.UseVisualStyleBackColor = true;
+            this.Convertbutton.Click += new System.EventHandler(this.Convertbutton_Click);
+            // 
+            // MorsecodelistBox
+            // 
+            this.MorsecodelistBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MorsecodelistBox.FormattingEnabled = true;
+            this.MorsecodelistBox.ItemHeight = 24;
+            this.MorsecodelistBox.Location = new System.Drawing.Point(157, 87);
+            this.MorsecodelistBox.Margin = new System.Windows.Forms.Padding(4);
+            this.MorsecodelistBox.Name = "MorsecodelistBox";
+            this.MorsecodelistBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.MorsecodelistBox.Size = new System.Drawing.Size(173, 124);
+            this.MorsecodelistBox.TabIndex = 8;
+            this.MorsecodelistBox.TabStop = false;
+            this.toolTip1.SetToolTip(this.MorsecodelistBox, "Text In Morse Code");
+            // 
+            // StandardtextBox
+            // 
+            this.StandardtextBox.Location = new System.Drawing.Point(49, 53);
+            this.StandardtextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.StandardtextBox.Name = "StandardtextBox";
+            this.StandardtextBox.Size = new System.Drawing.Size(386, 26);
+            this.StandardtextBox.TabIndex = 0;
+            this.StandardtextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 21);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(375, 19);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Enter the text that you want to be converted to Morse Code.";
+            // 
+            // Clrbutton
+            // 
+            this.Clrbutton.Location = new System.Drawing.Point(256, 219);
+            this.Clrbutton.Margin = new System.Windows.Forms.Padding(4);
+            this.Clrbutton.Name = "Clrbutton";
+            this.Clrbutton.Size = new System.Drawing.Size(112, 34);
+            this.Clrbutton.TabIndex = 2;
+            this.Clrbutton.Text = "C&lear";
+            this.toolTip1.SetToolTip(this.Clrbutton, "Clear All Values");
+            this.Clrbutton.UseVisualStyleBackColor = true;
+            this.Clrbutton.Click += new System.EventHandler(this.Clrbutton_Click);
+            // 
+            // Soundbutton
+            // 
+            this.Soundbutton.AccessibleRole = System.Windows.Forms.AccessibleRole.Equation;
+            this.Soundbutton.Location = new System.Drawing.Point(139, 219);
+            this.Soundbutton.Name = "Soundbutton";
+            this.Soundbutton.Size = new System.Drawing.Size(112, 34);
+            this.Soundbutton.TabIndex = 9;
+            this.Soundbutton.Text = "&Play Sound";
+            this.toolTip1.SetToolTip(this.Soundbutton, "Play Morse Code Sound");
+            this.Soundbutton.UseVisualStyleBackColor = true;
+            this.Soundbutton.Click += new System.EventHandler(this.Soundbutton_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.Convertbutton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Goldenrod;
+            this.CancelButton = this.Exitbutton;
+            this.ClientSize = new System.Drawing.Size(525, 276);
+            this.Controls.Add(this.Soundbutton);
+            this.Controls.Add(this.Clrbutton);
+            this.Controls.Add(this.Exitbutton);
+            this.Controls.Add(this.Convertbutton);
+            this.Controls.Add(this.MorsecodelistBox);
+            this.Controls.Add(this.StandardtextBox);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chapter 8 Jerry Nava Morse Code";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Exitbutton;
+        private System.Windows.Forms.Button Convertbutton;
+        private System.Windows.Forms.ListBox MorsecodelistBox;
+        private System.Windows.Forms.TextBox StandardtextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Clrbutton;
+        private System.Windows.Forms.Button Soundbutton;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ Jerry-Nava-CPT-185-Chapter-8/Form1.cs
@@ -0,0 +1,259 @@
+﻿//Jerry Nava
+//CPT-185-A01S
+//Lab 8
+using System;
+using System.Media;
+using System.Windows.Forms;
+
+namespace Jerry_Nava_CPT_185_Chapter_8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Convertbutton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string standlang = StandardtextBox.Text;
+                string morse = "";
+
+                for (int i = 0; i < standlang.Length; i++)
+                {
+
+                    switch (char.ToLower(standlang[i]))
+                    {
+
+                        case 'a': morse = ".- "; break;
+                        case 'b': morse = "-..." ; break;
+                        case 'c': morse = " -.-." ; break;
+                        case 'd': morse = " -.." ; break;
+                        case 'e': morse = "." ; break;
+                        case 'f': morse = "..-." ; break;
+                        case 'g': morse = "--." ; break;
+                        case 'h': morse = "...." ; break;
+                        case 'i': morse = ".." ; break;
+                        case 'j': morse = ".---" ; break;
+                        case 'k': morse = "-.-" ; break;
+                        case 'l': morse = ".-.." ; break;
+                        case 'm': morse = "--" ; break;
+                        case 'n': morse = "-." ; break;
+                        case 'o': morse = "---" ; break;
+                        case 'p': morse = "-.-." ; break;
+                        case 'q': morse = "--.-" ; break;
+                        case 'r': morse = ".-." ; break;
+                        case 's': morse = "..." ; break;
+                        case 't': morse = "-" ; break;
+                        case 'u': morse = "..-" ; break;
+                        case 'v': morse = "...-" ; break;
+                        case 'w': morse = ".--" ; break;
+                        case 'x': morse = "-..-" ; break;
+                        case 'y': morse = "-.--" ; break;
+                        case 'z': morse = "--.." ; break;
+                        case ' ': morse = " " ; break;
+                        case ',': morse = "--..--" ; break;
+                        case '.': morse = ".-.-.-" ; break;
+                        case '0': morse = "-----" ; break;
+                        case '1': morse = ".----" ; break;
+                        case '2': morse = "..---" ; break;
+                        case '3': morse = "...--" ; break;
+                        case '4': morse = "....-" ; break;
+                        case '5': morse = "....." ; break;
+                        case '6': morse = "-...." ; break;
+                        case '7': morse = "--..." ; break;
+                        case '8': morse = "---.." ; break;
+                        case '9': morse = "----." ; break;
+                    }
+                    MorsecodelistBox.Items.Add("             "+morse+"\n");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Exitbutton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Clrbutton_Click(object sender, EventArgs e)
+        {
+            StandardtextBox.Clear();
+            MorsecodelistBox.Items.Clear();
+            StandardtextBox.Focus();
+        }
+
+        private void Soundbutton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string standlang = StandardtextBox.Text;
+                for (int i = 0; i < standlang.Length; i++)
+                {
+                    switch (char.ToLower(standlang[i]))
+                    {
+                        case 'a':
+                            SoundPlayer sound = new SoundPlayer("A.wav");
+                            sound.PlaySync();
+                            ; break;
+                        case 'b':
+                            SoundPlayer B = new SoundPlayer("B.wav");
+                            B.PlaySync();
+                            ; break;
+                        case 'c':
+                            SoundPlayer C = new SoundPlayer("C.wav");
+                            C.PlaySync();
+                            ; break;
+                        case 'd':
+                            SoundPlayer D = new SoundPlayer("D.wav");
+                            D.PlaySync();
+                            ; break;
+                        case 'e':
+                            SoundPlayer E = new SoundPlayer("E.wav");
+                            E.PlaySync();
+                            ; break;
+                        case 'f':
+                            SoundPlayer F = new SoundPlayer("F.wav");
+                            F.PlaySync();
+                            ; break;
+                        case 'g':
+                            SoundPlayer G = new SoundPlayer("G.wav");
+                            G.PlaySync();
+                            ; break;
+                        case 'h':
+                            SoundPlayer H = new SoundPlayer("H.wav");
+                            H.PlaySync();
+                            ; break;
+                        case 'i':
+                            SoundPlayer I = new SoundPlayer("I.wav");
+                            I.PlaySync();
+                            ; break;
+                        case 'j':
+                            SoundPlayer J = new SoundPlayer("J.wav");
+                            J.PlaySync();
+                            ; break;
+                        case 'k':
+                            SoundPlayer K = new SoundPlayer("K.wav");
+                            K.PlaySync();
+                            ; break;
+                        case 'l':
+                            SoundPlayer L = new SoundPlayer("L.wav");
+                            L.PlaySync();
+                            ; break;
+                        case 'm':
+                            SoundPlayer M = new SoundPlayer("M.wav");
+                            M.PlaySync();
+                            ; break;
+                        case 'n':
+                            SoundPlayer N = new SoundPlayer("N.wav");
+                            N.PlaySync();
+                            ; break;
+                        case 'o':
+                            SoundPlayer O = new SoundPlayer("O.wav");
+                            O.PlaySync();
+                            ; break;
+                        case 'p':
+                            SoundPlayer P = new SoundPlayer("P.wav");
+                            P.PlaySync();
+                            ; break;
+                        case 'q':
+                            SoundPlayer Q = new SoundPlayer("Q.wav");
+                            Q.PlaySync();
+                            ; break;
+                        case 'r':
+                            SoundPlayer R = new SoundPlayer("R.wav");
+                            R.PlaySync();
+                            ; break;
+                        case 's':
+                            SoundPlayer S = new SoundPlayer("S.wav");
+                            S.PlaySync();
+                            ; break;
+                        case 't':
+                            SoundPlayer t = new SoundPlayer("T.wav");
+                            t.PlaySync();
+                            ; break;
+                        case 'u':
+                            SoundPlayer U = new SoundPlayer("U.wav");
+                            U.PlaySync();
+                            ; break;
+                        case 'v':
+                            SoundPlayer V = new SoundPlayer("V.wav");
+                            V.PlaySync();
+                            ; break;
+                        case 'w':
+                            SoundPlayer W = new SoundPlayer("W.wav");
+                            W.PlaySync();
+                            ; break;
+                        case 'x':
+                            SoundPlayer X = new SoundPlayer("X.wav");
+                            X.PlaySync();
+                            ; break;
+                        case 'y':
+                            SoundPlayer Y = new SoundPlayer("Y.wav");
+                            Y.PlaySync();
+                            ; break;
+                        case 'z':
+                            SoundPlayer Z = new SoundPlayer("Z.wav");
+                            Z.PlaySync();
+                            ; break;
+
+                        case ',':
+                            SoundPlayer comma = new SoundPlayer(",.wav");
+                            comma.PlaySync();
+                            ; break;
+
+                        case '0':
+                            SoundPlayer zero = new SoundPlayer("0.wav");
+                            zero.PlaySync();
+                            ; break;
+                        case '1':
+                            SoundPlayer one = new SoundPlayer("1.wav");
+                            one.PlaySync();
+                            ; break;
+                        case '2':
+                            SoundPlayer two = new SoundPlayer("2.wav");
+                            two.PlaySync();
+                            ; break;
+                        case '3':
+                            SoundPlayer three = new SoundPlayer("3.wav");
+                            three.PlaySync();
+                            ; break;
+                        case '4':
+                            SoundPlayer four = new SoundPlayer("4.wav");
+                            four.PlaySync();
+                            ; break;
+                        case '5':
+                            SoundPlayer five = new SoundPlayer("5.wav");
+                            five.PlaySync();
+                            ; break;
+                        case '6':
+                            SoundPlayer six = new SoundPlayer("6.wav");
+                            six.PlaySync();
+                            ; break;
+                        case '7':
+                            SoundPlayer seven = new SoundPlayer("7.wav");
+                            seven.PlaySync();
+                            ; break;
+                        case '8':
+                            SoundPlayer eight = new SoundPlayer("8.wav");
+                            eight.PlaySync();
+                            ; break;
+                        case '9':
+                            SoundPlayer nine = new SoundPlayer("9.wav");
+                            nine.PlaySync();
+                            ; break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
